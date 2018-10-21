bb1dda81e7df1d54bd604d7a220ba62654dac310
Allen <allendinghamilton@gmail.com>
2018-10-21 17:55:23 -0400

    added deploy script

---
+++ query1/impl_java/qr_code/deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+sudo apt-get -y install default-jdk
+
+sudo update-java-alternatives -l
+export JAVA_HOME='/usr/lib/jvm/java-1.11.0-openjdk-amd64'
+
+curl http://mirror.metrocast.net/apache/tomcat/tomcat-8/v8.5.34/bin/apache-tomcat-8.5.34.tar.gz > apace-tomcat.tar.gz
+
+tar xf apace-tomcat.tar.gz
+sudo mv -f apache-tomcat-8.5.34 /usr/local
+
+# change port to 80
+sed -i -- 's/8080/80/g' server.xml
+
+# deploy war
+sudo rm -r /usr/local/apache-tomcat-8.5.34/webapps/ROOT
+sudo cp ~/qr-code-1.0-SNAPSHOT.war /usr/local/apache-tomcat-8.5.34/webapps/ROOT.war
+
+# start tomcat
+sudo /usr/local/apache-tomcat-8.5.34/bin/startup.sh

d1e91dc7e2f227e314ba18c8ab3187800f4281f8
allenchou <allenchou7@gmail.com>
2018-10-30 22:48:12 -0400

    remove sql account

--- terraform/query2/MySQL/config_mysql.sql
+++ terraform/query2/MySQL/config_mysql.sql
@@ -1,4 +1,4 @@
 use mysql;
-create user 'spongebob'@'%' identified by '15619';
-grant all privileges on *.* to 'spongebob'@'%' identified by '15619' with grant option;
+create user '<SQL_USER>'@'%' identified by '<SQL_PASSWORD>';
+grant all privileges on *.* to '<SQL_USER>'@'%' identified by '<SQL_PASSWORD>' with grant option;
 flush privileges;
--- terraform/query2/MySQL/script.sh
+++ terraform/query2/MySQL/script.sh
@@ -5,21 +5,14 @@ sudo apt install -y mysql-server
 sleep 30
 sudo mysql < config_mysql.sql
 echo "
-# romote access
 [mysqld]
 bind-address = 0.0.0.0
-
-# 💩 𝌆
-# UTF-8 should be used instead of Latin1. Obviously.
-# NOTE "utf8" in MySQL is NOT full UTF-8: http://mathiasbynens.be/notes/mysql-utf8mb4
-
-[client]
-default-character-set = utf8mb4
-
-[mysqld]
 character-set-client-handshake = FALSE
 character-set-server = utf8mb4
 collation-server = utf8mb4_bin
+
+[client]
+default-character-set = utf8mb4
 
 [mysql]
 default-character-set = utf8mb4
@@ -42,8 +35,8 @@ echo "
 export MYSQL_DB_NAME=twitter
 export MYSQL_DNS=localhost
 export MYSQL_DNS=localhost
-export MYSQL_USER=spongebob
-export MYSQL_PWD=15619
+export MYSQL_USER=<SQL_USER>
+export MYSQL_PWD=<SQL_PASSWORD>
 " | sudo tee --append /var/lib/tomcat8/bin/setenv.sh
 
 # DANGEROUS

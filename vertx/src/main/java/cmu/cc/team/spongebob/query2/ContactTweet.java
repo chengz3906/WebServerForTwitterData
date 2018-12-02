f3c73a71f536fba606b364b98184c7ae904d2104
chengz3906 <cehngz3906@gmail.com>
2018-12-01 21:50:01 -0500

    removed tweet priority queue

--- vertx/src/main/java/cmu/cc/team/spongebob/query2/ContactTweet.java
+++ vertx/src/main/java/cmu/cc/team/spongebob/query2/ContactTweet.java
@@ -19,10 +19,10 @@ public class ContactTweet implements Comparable<ContactTweet> {
 
     @Override
     public int compareTo(ContactTweet other) {
-        if (this.phraseCount != other.getPhraseCount()) {
-            return other.getPhraseCount() - this.phraseCount;
+        if (this.phraseCount != other.phraseCount) {
+            return other.phraseCount - this.phraseCount;
         } else {
-            return other.getCreatedAt().compareTo(this.createdAt);
+            return other.createdAt.compareTo(this.createdAt);
         }
     }
 
